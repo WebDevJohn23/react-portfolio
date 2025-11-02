612dbdcfb1c46de7c64143620d89481f2ccd9c6c
Johnathan Julig <Johnathan.Julig@gmail.com>
2025-11-01 22:27:33 -0700

    perf: update Project component with cache-busting API calls

--- src/api/getProjects.js
+++ src/api/getProjects.js
@@ -1,6 +1,6 @@
 export async function getProjects() {
     const url =
-        "https://portfolio.johnathanjulig.com/wp-json/wp/v2/project?per_page=100&_embed&acf_format=standard";
+        `https://portfolio.johnathanjulig.com/wp-json/wp/v2/project?per_page=100&_embed&acf_format=standard&timestamp=${Date.now()}`;
 
     const res = await fetch(url, { headers: { Accept: "application/json" } });
     if (!res.ok) throw new Error(`WP API error: ${res.status}`);
