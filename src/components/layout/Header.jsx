bc7d7dbb3108f45d230ae402216cf0be1b54ec7a
Johnathan Julig <Johnathan.Julig@gmail.com>
2025-11-01 17:21:08 -0700

    feat: add header and css

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,17 @@
 export default function Header() {
     return (
-        <header>
-            <h1>Project Name</h1>
+        <header className='header'>
+            <div className="logo padding-20">
+                <a href="#">Johnathan Julig</a>
+            </div>
+            <div className="header-right">
+<ul>
+    <li className="menu-item padding-20"><a href="#stack">Stack</a></li>
+    <li className="menu-item padding-20"><a href="#projects">Projects</a></li>
+    <li className="menu-item padding-20"><a href="#about">About</a></li>
+    <li className="menu-item padding-20"><a href="#contact">Contact</a></li>
+</ul>
+            </div>
         </header>
     );
 }
