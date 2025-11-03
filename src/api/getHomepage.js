1936276e45baae3d29af1959068b1278307662ef
Johnathan Julig <Johnathan.Julig@gmail.com>
2025-11-03 13:56:12 -0800

    feat: add about component

--- src/api/getHomepage.js
+++ src/api/getHomepage.js
@@ -1,5 +1,5 @@
-// src/api/getHero.js
-export async function getHero() {
+// src/api/getHomepage.js
+export async function getHomepage() {
   const url = `https://portfolio.johnathanjulig.com/wp-json/wp/v2/pages/94?acf_format=standard&timestamp=${Date.now()}`;
 
   const res = await fetch(url, { headers: { Accept: 'application/json' } });
--- src/components/layout/MainContent.jsx
+++ src/components/layout/MainContent.jsx
@@ -1,14 +1,16 @@
 import Projects from '../sections/Projects.jsx';
 import Hero from '../sections/Hero.jsx';
 import Stack from '../sections/Stack.jsx';
+import About from '../sections/About.jsx';
 import './MainComponent.scss';
 
 export default function MainContent() {
   return (
-    <main>
+    <main className="mainContent">
       <Hero />
       <Stack />
       <Projects />
+      <About />
     </main>
   );
 }
