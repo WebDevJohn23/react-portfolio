624de5d53e3dcfdd09bbc019e78f53dbccc546bf
Johnathan Julig <Johnathan.Julig@gmail.com>
2025-11-03 22:18:43 -0800

    fix: configure Vite to load environment variables using dotenv

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), 'VITE_');
+// Load env vars
+dotenv.config();
 
-  console.log('Loaded env vars:', {
-    serviceId: env.VITE_EMAILJS_SERVICE_ID,
-    templateId: env.VITE_EMAILJS_TEMPLATE_ID,
-    publicKey: env.VITE_EMAILJS_PUBLIC_KEY,
-  });
-
-  return {
-    plugins: [react()],
-  };
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    'import.meta.env.VITE_EMAILJS_SERVICE_ID': JSON.stringify(process.env.VITE_EMAILJS_SERVICE_ID),
+    'import.meta.env.VITE_EMAILJS_TEMPLATE_ID': JSON.stringify(
+      process.env.VITE_EMAILJS_TEMPLATE_ID,
+    ),
+    'import.meta.env.VITE_EMAILJS_PUBLIC_KEY': JSON.stringify(process.env.VITE_EMAILJS_PUBLIC_KEY),
+  },
 });
